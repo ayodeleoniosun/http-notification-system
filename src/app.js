d464c58a80ba82f7bcf06f7608b5d7ccf3282f38
Dev-AY <heywire4u@gmail.com>
2021-04-04 23:16:56 +0100

    Set up app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 import config from 'config'
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
+
+const allowed_origins = ['http://localhost:3000']
+
+app.use( (req, res, next) => {
+  const origin = req.headers.origin
+
+  if (allowed_origins.indexOf(origin) > -1)
+    res.setHeader('Access-Control-Allow-Origin', origin)
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  next()
+})
+
+require('./routes')(app)
 
 app.listen(config.get('port'), () => {
   console.log(`${config.get('appName')} is currently running on port ${config.get('port')}`)
