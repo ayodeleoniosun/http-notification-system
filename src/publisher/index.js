a50b2c3b51bee27d6e11c70f6c7c643b48bd40bd
Dev-AY <heywire4u@gmail.com>
2021-04-05 10:46:04 +0100

    Create seperate folder for publisher

---
+++ src/publisher/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import config from 'config'
+import docsRouter from '../../docs/index'
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
+
+const publisher_controller = require('../controllers/publisher')
+app.post('/publish/:topic', publisher_controller.publish)
+app.use('/docs', docsRouter)
+
+app.listen(config.get('port'), () => {
+  console.log(`Publisher is currently running on port ${config.get('port')}`)
+})
