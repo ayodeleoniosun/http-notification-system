c26bf2cf158ce83b719e2549fd5872a06c7d3043
Dev-AY <heywire4u@gmail.com>
2021-04-05 09:05:18 +0100

    Create subscription

--- src/services/subscriber.js
+++ src/services/subscriber.js
@@ -3,12 +3,19 @@ import customErrorCodes from '../constants/customErrorCodes'
 import {CustomError} from '../helpers/errors'
 const subscriber = redis.createClient()
 
-export default class SubscriberService {
+class SubscriberService {
   subscribe (payload, topic) {
-    const token = subscriber.subscribe(topic, JSON.stringify(payload))
-    
-    if (token)
+    const subscription = subscriber.subscribe(topic, (message, channel) => {
+      console.log(`Received messages in ${channel} : ${JSON.stringify(payload)}`)
+    })
+
+    const subscribe = subscriber.subscribe(topic, subscription)
+
+    if (subscribe)
       return {url: payload.url, topic: topic}
     throw new CustomError(customErrorCodes.SUBSCRIPTION_NOT_CREATED)
   }
 }
+
+export default SubscriberService
+
