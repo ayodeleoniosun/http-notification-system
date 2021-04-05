05ee709e90c615ecda3f72e44e33d201b02bc870
Dev-AY <heywire4u@gmail.com>
2021-04-05 10:46:22 +0100

    Remove cors limit from subscriber file

--- src/subscribers/index.js
+++ src/subscribers/index.js
@@ -7,19 +7,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors())
 
-const allowed_origins = ['http://localhost:3000']
-
-app.use( (req, res, next) => {
-  const origin = req.headers.origin
-
-  if (allowed_origins.indexOf(origin) > -1)
-    res.setHeader('Access-Control-Allow-Origin', origin)
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  next()
-})
-
 class Subscriber {
   constructor(port) {
     this.port = port
